c68fdffcc69017a733ef8eb49beca5aee2a14eb0
neerajkumar1989 <neeraj@stepforwardsolution.com>
2021-05-16 23:53:56 +0530

    Master Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users
-    get '/api/typeahead/:search', to: 'users#typehead'
+    get 'typehead/:search', to: 'users#typehead'
   end
 end
